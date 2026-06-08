a979f998dccd5e74ea4e9a8d8276319d6eff318d
critical_error <nikula2108@gmail.com>
2026-06-08 22:02:36 +0300

    chore(db): add entities and their configurations to the database context.

---
+++ GameList.DataAccess/GameListDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GameList.Core.Models;
+using GameList.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameList.DataAccess;
+
+public class GameListDbContext(DbContextOptions<GameListDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<GameEntity> Games { get; set; }
+    public DbSet<CollectionEntity> Collections { get; set; }
+    public DbSet<TagEntity> Tags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new GameConfiguration());
+        modelBuilder.ApplyConfiguration(new CollectionConfiguration());
+        modelBuilder.ApplyConfiguration(new TagConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
