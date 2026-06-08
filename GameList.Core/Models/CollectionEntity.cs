3fd6c3446c2b9e70cd181c60b98ad1e247c62ca4
critical_error <nikula2108@gmail.com>
2026-06-08 20:02:26 +0300

    feat(db): implement database entity models.

---
+++ GameList.Core/Models/CollectionEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace GameList.Core.Models;
+public class CollectionEntity
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public List<GameEntity> Games { get; set; } = [];
+}
---
+++ GameList.Core/Models/GameEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace GameList.Core.Models;
+
+public class GameEntity
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Progress { get; set; } = 0;
+    public int Rating { get; set; } = 0;
+    public string CoverURL { get; set; } = string.Empty;
+    public List<TagEntity> Tags { get; set; } = [];
+    public List<CollectionEntity> Collections { get; set; } = [];   
+}
