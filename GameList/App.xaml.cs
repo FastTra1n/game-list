dba3be96dd98213d63f7ba56d246586ec4abef8c
critical_error <nikula2108@gmail.com>
2026-06-09 22:10:36 +0300

    feat(db): add database initialization before launching the application.

--- GameList/App.xaml.cs
+++ GameList/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using GameList.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +12,27 @@ namespace GameList
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<GameListDbContext>(options =>
+                options.UseSqlite("Data Source=gamelist.db"));
+            serviceCollection.AddTransient<MainWindow>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<GameListDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 
 }
