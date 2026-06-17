350b0dd92d926ab79d85441fb10ef01b6b606874
critical_error <nikula2108@gmail.com>
2026-06-17 20:27:31 +0300

    feat(ViewModel): add fetching of the list of games from db.

---
+++ GameList/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GameList.Core.Models;
+using GameList.DataAccess.Repositories;
+using System.Collections.ObjectModel;
+
+namespace GameList.ViewModels;
+
+public class MainViewModel
+{
+    public readonly GamesRepository _repository;
+    public ObservableCollection<GameEntity> Games { get; set; } = [];
+
+    public MainViewModel(GamesRepository repository)
+    {
+        _repository = repository;
+        _ = LoadAsync();
+    }
+
+    public async Task LoadAsync()
+    {
+        List<GameEntity> gamesList = await _repository.Get();
+        Games.Clear();
+        foreach (var game in gamesList)
+        {
+            Games.Add(game);
+        }
+    }
+}
